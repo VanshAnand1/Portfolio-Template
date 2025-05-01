7ded506c8037e338519fa345f50021e12faa81df
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-05-01 00:29:29 -0400

    HashRouter

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,34 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
 import { NotFound } from "./pages/notfound";
 
-// Fixes GitHub Pages refresh issue on subroutes
-function RedirectHandler() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const redirectPath = urlParams.get("redirect");
-
-    if (redirectPath) {
-      navigate(redirectPath, { replace: true });
-    }
-  }, [navigate]);
-
-  return null;
-}
-
 function App() {
   return (
-    <BrowserRouter basename="/Portfolio">
-      <RedirectHandler />
+    <HashRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
