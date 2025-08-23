3d6123e1804e27b63f9fbee6ea37f4d0cd651f86
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-08-23 02:22:55 -0400

    2/ feat: added hero card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { NavigationBar } from "./components/NavigationBar";
 import { LightRaysBackground } from "./components/LightRaysBackground";
+import { SplineSceneBasic } from "./components/Home";
 
 function App() {
   return (
     <div>
       <LightRaysBackground />
       <NavigationBar />
+      <SplineSceneBasic />
     </div>
   );
 }
