5958377f06b8a70e76f1e55255eecbbdf0544829
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-08-23 14:40:49 -0400

    2/ feat: centered hero card and changed navbar logo

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -6,7 +6,7 @@ import { Spotlight } from "./elements/Interactive3DHero/spotlight";
 
 export function SplineSceneBasic() {
   return (
-    <Card className="center-h h-[500px] bg-black/[0.96] relative overflow-hidden">
+    <Card className="center-h h-[500px] bg-black/[0.96] relative top-40 overflow-hidden">
       <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" />
       <div className="flex h-full">
         {/* Left content */}
