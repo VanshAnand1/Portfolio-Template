9cc748ee7fbc784cf77d472f3e5c5030cb12a1b4
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-08-24 03:42:26 -0400

    5/ feat: confetti button

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import { ConfettiButton } from "./lightswind/confetti-button";
+import { Mailbox } from "lucide-react";
+
 export const Contact = () => {
-  return <div>Get in Touch!</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+  };
+
+  return (
+    <div className="mt-40 flex justify-center">
+      <div className="text-white">hi</div>
+      <ConfettiButton
+        icon={<Mailbox className="h-4 w-4" />}
+        variant="gradient"
+        size="lg"
+        animation="glow"
+        onClick={handleSubmit}
+        confettiOptions={{
+          particleCount: 100,
+          spread: 70,
+        }}
+      >
+        Get in Touch!
+      </ConfettiButton>
+    </div>
+  );
 };
